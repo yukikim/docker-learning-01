fc2b99788cbdd594c70874e107bce93555403f45
yukikim <tki6ra@gmail.com>
2026-01-08 14:02:38 +0900

    ルート(URL)を追加

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def hello():
     count = redis.incr('hits')
     return render_template('index.html', message=f'こんにちは！あなたは {count} 番目の訪問者です。')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8800)
+    app.run(host="0.0.0.0", port=8800, debug=True)
